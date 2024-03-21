dc058d9c3aff1ac578d5a79c97c9ae246387d018
DrimJimbo <117316821+DrimJimbo@users.noreply.github.com>
2024-03-21 17:04:24 +0100

    scraping athlete
    scraping athlete
    doctest de record

---
+++ Scraping/athlete.py
@@ -0,0 +1,26 @@
+###################
+# Script de scraping Athlete
+###################
+
+##### Import #####
+import doctest
+import requests
+from bs4 import BeautifulSoup
+import ast
+
+##### Code #####
+list_record = []  # Dict où chaque clé représente un attribut de la table record
+
+def contenue(url):
+    '''
+    Permet de récupérer le contenue d'un URL
+    param : (str) url
+    return : Soup
+    '''
+    requete = requests.get(url) # Envoie une requete get a l'url
+    # verifie si la requete a reussi
+    if requete.status_code != 200 :
+        return("La requete n'as pas aboutis")
+    cont = requete.text # contenue brut de l'url, cad balise, script, commentaire
+    soup = BeautifulSoup(cont,"html.parser") #contenue de l'url, cad uniquement html (balise)
+    return soup
--- Scraping/record.py
+++ Scraping/record.py
@@ -26,6 +26,11 @@ def contenue(url):
     return soup
 
 def scraping_athletisme(soup):
+    '''
+    Retourne la liste de dictionnaire de althletisme 
+    param : Objet BeautifulSoup
+    return : list(dict)
+    '''
     liste = []
     tables = soup.find_all("table")
     for i in range (0,3):
@@ -39,6 +44,11 @@ def scraping_athletisme(soup):
     return liste
 
 def contient_n_fois(ch,c,nbr):
+    '''
+    Renvoie True si la chaine de caractere ch possede nbr caractere c
+    param : (str) ch -> chaine de caractere , (str) c -> caractere, (int) nbr -> nombre d'iteration
+    return : Boolean 
+    '''
     n = 0
     for char in ch :
         if c == char :
@@ -46,10 +56,20 @@ def contient_n_fois(ch,c,nbr):
     return n >= nbr
     
 def selection(ch):
+    '''
+    Renvoie ch sans le mot "Progression"
+    param : (str) ch -> chaine de caractere
+    return : (str) ch
+    '''
     index = ch.index('Progression')
     return ch[0:index]
 
 def remplir_dict(liste , sport):
+    '''
+    Permet de remplir le liste de dict
+    param : (list(list)) liste -> liste de scraping, (str) sport 
+    return : (list(dict)) liste_dict
+    '''
     liste_dict = []
     for l in liste :
         dict = {"stat_record" : l[3],"date_record" : l[4],"id_event" : None,"id_athele" : None, "discipline" : sport , "event" : l[0], "athlete" : l[1]}
@@ -57,6 +77,11 @@ def remplir_dict(liste , sport):
     return liste_dict
 
 def scraping_natation(soup):
+    '''
+    Retourne la liste de dictionnaire de natation
+    param : Objet BeautifulSoup soup
+    return : list(dict) liste 
+    '''
     liste = []
     tables = soup.find_all("table")
     for i in range(7,10) :
@@ -71,6 +96,9 @@ def scraping_natation(soup):
 
 ##### Main #####
 def main():
+    '''
+    Fonction principal
+    '''
     # ATHLETISME 
     soup = contenue("https://fr.wikipedia.org/wiki/Records_olympiques_d%27athl%C3%A9tisme")
     liste = scraping_athletisme(soup)

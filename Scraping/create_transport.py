02f2b022599cf33920e7fcb16406d08ffbdebf1c
En3ide <38477527+En3ide@users.noreply.github.com>
2024-03-22 00:02:16 +0100

    Create create_transport.py

---
+++ Scraping/create_transport.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests, ast, sqlite3, re, locale
+from datetime import datetime
+
+url = "https://olympics.com/fr/paris-2024/sites"
+
+def recup_url_transp(url):
+    reponse = requests.get(url)
+    print("test2")
+    if reponse.status_code == 200:
+        soup = BeautifulSoup(reponse.text, 'html.parser')
+
+        # Extraction du tableau
+        tab = soup.find_all('table', class_=['content-list-grid'])
+        print("test3")
+        print(tab[0])
+
+recup_url_transp(url)

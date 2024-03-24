88aac52d957c22080c181b154b838c4c9cfc8a77
En3ide <38477527+En3ide@users.noreply.github.com>
2024-03-24 18:39:07 +0100

    Update create_list_To_Serve.py

--- Scraping/create_list_To_Serve.py
+++ Scraping/create_list_To_Serve.py
@@ -6,9 +6,9 @@ url_tmp = "https://olympics.com/fr/paris-2024/sites"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"}
 
 def recup_url_transp(url):
-    print("test1")
+    #print("test1")
     reponse = requests.get(url, headers=HEADERS)
-    print("test2")
+    #print("test2")
     if reponse.status_code == 200:
         soup = BeautifulSoup(reponse.text, 'html.parser')
         result = []
@@ -21,8 +21,8 @@ def recup_url_transp(url):
                 #print(href.split("/")[-1].replace("-", " "))
                 if href != None:
                     #print(href)
-                    print(href.split("/")[-1])
-                    print(recup_transp(href))
+                    #print(href.split("/")[-1])
+                    #print(recup_transp(href))
                     if len(recup_transp(href)) >= 1:
                         result.append({"name_site": href.split("/")[-1].replace("-", " "), "transport": recup_transp(href)})
         print("Fini !!!")
@@ -75,8 +75,9 @@ def recup_info(sec):
                 sec = sec.replace(sec[:-sec[::-1].find("«")-1], "")
                 tmp = sec[sec.find("«")+2:sec.find("»")-1]
                 tmp2 = sec[sec.find("» (")+3:sec.find(")")]
-                if ", " in tmp2:
-                    tmp2 = tmp2.split(", ")
+                if ", " in tmp2 or " et " in tmp2:
+                    tmp2 = re.split(r',| et ', tmp2)
+                    tmp2 = [element.strip() for element in tmp2 if element.strip()]
                 else:
                     tmp2 = [tmp2]
                 temp = {"name": tmp, "number": tmp2}
@@ -86,16 +87,34 @@ def recup_info(sec):
                 contient_donne = False
     return result
 
+
+def dic_to_table(result):
+    tab = []
+    for site in result:
+        for station in site.get("transport"):
+            for number in station.get("number"):
+                if "RER" in number or "Train" in number:
+                    trsp_type = 1
+                elif "Tramway" in number:
+                    trsp_type = 2
+                elif "Bus" in number:
+                    trsp_type = 3
+                elif "Métro" in number or "Ligne" in number:
+                    trsp_type = 4
+                tab.append({"name_site": site.get("name_site"), "id_trans": trsp_type, "num_ligne": number, "station_name": station.get("name")})
+    return tab
+
+
 def send_site(result, bdd=""):
-    
+
     # Création de la requête SQL
-    send = "INSERT INTO Site_table (name_site, adress_site, creation_date_site, capacity_site, URL_site) VALUES\n"
+    send = "INSERT INTO To_Serve (id_site, id_trans, num_ligne, station_name) VALUES\n"
     for dic in result:
-        send += (" ('" + dic.get("name_site") + "', '" +
-            dic.get("adress_site") + "', '" +
-            dic.get("creation_date_site") + "', '" +
-            dic.get("capacity_site") + "', '" +
-            dic.get("URL_site") + "'),\n")
+        #for number in dic.get("number")
+        send += (" ('" + dic.get("id_site") + "', '" +
+            dic.get("id_trans") + "', '" +
+            dic.get("num_ligne") + "', '" +
+            dic.get("station_name") + "'),\n")
     send += send[:-2] + ";"
 
     if len(bdd) > 0:
@@ -107,4 +126,5 @@ def send_site(result, bdd=""):
         connexion.close()
     return(send)
 
-recup_url_transp(url_tmp)
+test = recup_url_transp(url_tmp)
+print(dic_to_table(test))

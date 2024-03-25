39580282cfbe95c4e71a4af58107a8e677b16839
En3ide <38477527+En3ide@users.noreply.github.com>
2024-03-25 21:59:58 +0100

    yaaaaaaaaaaaaaaa

--- Scraping/create_list_Is_from.py
+++ Scraping/create_list_Is_from.py
@@ -13,7 +13,7 @@ import requests, ast, sqlite3, re, locale
 main_url= "https://olympics.com/fr/paris-2024/sites"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"}
 
-def recup_url_transp(athlete, country):
+def recup_is_from(athlete, country):
     result = []
     for ath in athlete:
         result.append({"id_athlete": ath.index(), "code_country": ath.get("code_country")})
@@ -22,14 +22,12 @@ def recup_url_transp(athlete, country):
     print('[',datetime.now().time(),'] ', "sql Is_from fini !!!")
     return result
 
-def send_is_from(result):
+def send_is_from(result, bdd=""):
         # Création de la requête SQL
-    send = "INSERT INTO Is_from (id_athlete, ) VALUES\n"
+    send = "INSERT INTO Is_from (id_athlete, code_country) VALUES\n"
     for dic in result:
-        send += ("('" + dic.get("stat_record") + "', '" +
-            dic.get("date_record") + "', '" +
-            dic.get("id_event") + "', " +
-            dic.get("id_athlete") + "'),\n")
+        send += ("('" + dic.get("id_athlete") + "', '" +
+            dic.get("code_country") + "'),\n")
     send = send[:-2] + ";"
 
     if len(bdd) > 0:
@@ -41,3 +39,9 @@ def send_is_from(result):
         connexion.close()
     print('[',datetime.now().time(),'] ', "sql Record fini !!!")
     return(send)
+
+def create_sql(athlete, country):
+    return send_is_from(recup_is_from(athlete, country))
+
+if __name__ == "__main__":
+    print(recup_is_from())

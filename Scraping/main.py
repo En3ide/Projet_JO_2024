c4d3c6d3e9b5f24db8d198d294f2f0ad885497cf
En3ide <38477527+En3ide@users.noreply.github.com>
2024-03-25 16:03:21 +0100

    Le zeub a Jean Yves

--- Scraping/main.py
+++ Scraping/main.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process, Queue, Pool
+from multiprocessing import Process, Queue, Pool, subprocess
 from create_list_Transport import *
 from create_list_Site import *
 from create_list_Athlete import *
@@ -7,9 +7,26 @@ from create_list_Discipline import *
 from create_list_Record import *
 from create_list_To_Serve import *
 from create_list_Event import *
-from create_list_Date_calendar import *
+from create_list_Date_calendar import * 
+
+def installer_requirements(fichier_requirements):
+    """
+    Installe les dépendances spécifiées dans un fichier requirements.txt
+    Args:
+    - fichier_requirements (str): Le chemin vers le fichier requirements.txt
+    Returns:
+    - bool: True si l'installation s'est déroulée avec succès, False sinon
+    """
+    try:
+        subprocess.check_call(["pip", "install", "-r", fichier_requirements])
+        print("Installation des dépendances réussie.")
+        return True
+    except subprocess.CalledProcessError as e:
+        print("Erreur lors de l'installation des dépendances :", e)
+        return False
 
 if __name__ == "__main__":
+    installer_requirements("requirements.txt")
     with Pool() as pool:
         transport = pool.apply(recup_transport)
         site = pool.apply(recup_site)
